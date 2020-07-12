2822e84b5460e3a64ee22af8f3bb05819bf960dd
Marwan <zouinkhi.marwan@gmail.com>
2020-07-12 16:09:48 +0100

    release

--- src/main/kotlin/com/preibisch/pinna2d/util/DEFAULT.kt
+++ src/main/kotlin/com/preibisch/pinna2d/util/DEFAULT.kt
@@ -5,11 +5,12 @@ import com.preibisch.pinna2d.view.MainAnnotationView
 
 //val LUT_PATH = MainAnnotationView::class.java.getResource("/glasbey_inverted.lut").path
 
-//val INPUT_FOLDER = ""
-//val MASKS_FOLDER = ""
-//var PROJECT_FOLDER = ""
-var LUT_PATH = "/Users/Marwan/Desktop/glasbey_inverted.lut"
-val INPUT_FOLDER = "/Users/Marwan/Desktop/Irimia Project/Data/raw_Tiff"
-val MASKS_FOLDER = "/Users/Marwan/Desktop/Irimia Project/Data/Mask/masks"
-var PROJECT_FOLDER = "/Users/Marwan/Desktop/Irimia Project/"
+val INPUT_FOLDER = ""
+val MASKS_FOLDER = ""
+var PROJECT_FOLDER = ""
+var LUT_PATH = ""
+//var LUT_PATH = "/Users/Marwan/Desktop/glasbey_inverted.lut"
+//val INPUT_FOLDER = "/Users/Marwan/Desktop/Irimia Project/Data/raw_Tiff"
+//val MASKS_FOLDER = "/Users/Marwan/Desktop/Irimia Project/Data/Mask/masks"
+//var PROJECT_FOLDER = "/Users/Marwan/Desktop/Irimia Project/"
 var CURRENT_IMAGE = ""
